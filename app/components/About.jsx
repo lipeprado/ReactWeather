7771aad94cbecc6163ff4761ae95521725c0be68
lipeprado <fi-lipe2010@hotmail.com>
2017-02-25 14:39:41 -0300

    Updatind Pages

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,12 +1,12 @@
 var React = require('react');
 
 var About = (props) => {
-    return (
-        <div>
-            <h3>About</h3>
-            <p>Know more about us</p>
-        </div>
-    );
+  return (
+    <div>
+      <h3>About</h3>
+      <p>Welcome to the about page.</p>
+    </div>
+  )
 };
 
 module.exports = About;
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,12 +1,12 @@
 var React = require('react');
 
 var Examples = (props) => {
-    return (
-        <div>
-            <h3>Examples Page</h3>
-            <p>See here some weather cities examples.</p>
-        </div>
-)
+  return (
+    <div>
+      <h3>Examples</h3>
+      <p>Welcome to examples page!</p>
+    </div>
+  )
 };
 
 module.exports = Examples;
